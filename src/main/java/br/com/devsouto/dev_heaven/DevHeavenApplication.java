b10ef6c012c650d0407a2fb57d7881a279c3f843
Mestre Santiago <souto.santiago@academico.ifpb.edu.br>
2024-03-31 02:49:06 -0300

    layout inicial

---
+++ src/main/java/br/com/devsouto/dev_heaven/DevHeavenApplication.java
@@ -0,0 +1,13 @@
+package br.com.devsouto.dev_heaven;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevHeavenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevHeavenApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/devsouto/dev_heaven/DevHeavenApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.devsouto.dev_heaven;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevHeavenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
