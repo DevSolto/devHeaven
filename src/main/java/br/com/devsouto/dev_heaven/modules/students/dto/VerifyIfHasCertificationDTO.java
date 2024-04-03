91259c2d548a165f8f7253a3950c1ab19f29574b
Santiago Souto <86037575+DevSolto@users.noreply.github.com>
2024-04-03 21:10:18 +0000

    docker compose

--- src/main/java/br/com/devsouto/dev_heaven/modules/students/dto/VerifyIfHasCertificationDTO.java
+++ src/main/java/br/com/devsouto/dev_heaven/modules/students/dto/VerifyIfHasCertificationDTO.java
@@ -2,31 +2,15 @@ package br.com.devsouto.dev_heaven.modules.students.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-@Data
+
 @AllArgsConstructor
 @NoArgsConstructor
+@Data
 public class VerifyIfHasCertificationDTO {
 
     private String email;
 
     private String technology;
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getTechnology() {
-        return technology;
-    }
-
-    public void setTechnology(String technology) {
-        this.technology = technology;
-    }
 }
